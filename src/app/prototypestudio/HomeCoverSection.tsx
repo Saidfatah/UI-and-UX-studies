492271d9d8e159ebd80ce6a5f3f4059f4a3af738
Saidfatah <said_designer@outlook.com>
2026-08-28 18:33:12 +0100

    Prototype studio : cover animation wip

--- src/app/prototypestudio/HomeCoverSection.tsx
+++ src/app/prototypestudio/HomeCoverSection.tsx
@@ -1,4 +1,16 @@
+
+import { useEffect, useState } from "react";
+import HomeCoverTitle from "./HomeCoverTitle";
+
 function HomeCoverSection() {
+    const [showHomeTitle, setshowHomeTitle] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setshowHomeTitle(true);
+        }, 2000);
+    }, []);
+
     return (<section className="home-cover relative w-full h-screen pb-15 text-white overflow-hidden">
         <div className="home-cover-background absolute-full"
             style={{ translate: "none", rotate: "none", scale: "none", transform: "translate(0px, 0px)" }}>
@@ -12,7 +24,25 @@ function HomeCoverSection() {
             />
             <div className="absolute-full bg-black/30"></div>
         </div>
-        
+        <div className="relative grid-w content-end h-full z-1">
+            <div className="col-span-full flex justify-center mb-10">
+                {showHomeTitle && <HomeCoverTitle />}
+            </div>
+            <div className="home-cover-line col-span-full h-px bg-white/20 origin-center" />
+            <div className="col-span-full flex justify-center mt-15">
+                <div className="home-cover-scroll body-16 md:body-20 italic font-heading cursor-pointer" >
+                    <span className="char" aria-hidden="true" >S</span>
+                    <span className="char" aria-hidden="true" >c</span>
+                    <span className="char" aria-hidden="true" >r</span>
+                    <span className="char" aria-hidden="true" >o</span>
+                    <span className="char" aria-hidden="true" >l</span>
+                    <span className="char" aria-hidden="true" >l</span>
+                </div>
+            </div>
+        </div>
+
+
+
     </section>);
 }
 
--- src/app/prototypestudio/page.tsx
+++ src/app/prototypestudio/page.tsx
@@ -1,17 +1,37 @@
 "use client";
 import Header from "./header";
 import HomeCoverSection from "./HomeCoverSection";
+import HomeCoverTitle from "./HomeCoverTitle";
 import "./style.css";
+
+import { CustomEase } from "gsap/CustomEase";
+import {gsap} from "gsap";
+import { useEffect, useState } from "react";
+
+gsap.registerPlugin(CustomEase);
+
+CustomEase.create("beaucoup.alpha", ".25, .46, .45, .9");
+
+CustomEase.create(
+    "beaucoup.inOut",
+    "M0,0 C0.094,0.026 0.124,0.127 0.157,0.29 0.197,0.486 0.243,0.81 0.337,0.894 0.409,0.959 0.374,1 1,1"
+);
+
+CustomEase.create(
+    "beaucoup.slowInOut",
+    "M0,0 C0.2,0 0,1 1,1"
+);
 
 function PrototypeStudio() {
 
+    
 
     return (
         <>
             <Header />
             <div className="main content">
-                <div className="home">
-                    <HomeCoverSection/>
+                <div className="home h-screen flex justify-center items-center">
+                    <HomeCoverSection />
                 </div>
             </div>
         </>
